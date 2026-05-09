90ea0599a78fb7d711632b2cc1f28e64119bce71
Kamiel de Visser <kamieldevisser@gmail.com>
2026-05-09 12:27:52 +0200

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "codevogel's neovim config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    neovim-nightly-overlay = {
+      url = "github:nix-community/neovim-nightly-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    mnw.url = "github:gerg-l/mnw";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      mnw,
+      self,
+      ...
+    }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+
+      config = import ./config.nix {
+        inherit pkgs inputs;
+      };
+    in
+    {
+      packages.${system} = {
+        default = mnw.lib.wrap pkgs config;
+        dev = self.packages.${system}.default.devMode;
+      };
+    };
+}
