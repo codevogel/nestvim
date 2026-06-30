7a149f2ff1e30917ed80951bd85bd7516ad92387
Kamiel de Visser <kamieldevisser@gmail.com>
2026-06-30 11:10:05 +0200

    add swiftpick lualine component

--- nvim/lua/codevogel/plugins/lazy/lualine.lua
+++ nvim/lua/codevogel/plugins/lazy/lualine.lua
@@ -25,7 +25,10 @@ return {
         sections = {
           lualine_a = { "mode" },
           lualine_b = { "branch", "diff", "diagnostics" },
-          lualine_c = { "filename", require("swiftpick.lualine").component() },
+          lualine_c = {
+            "filename",
+            { require("swiftpick.lualine").component() },
+          },
           lualine_x = { "encoding", "fileformat", "filetype" },
           lualine_y = { "progress" },
           lualine_z = { "location" },
